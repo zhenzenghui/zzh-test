a2f19c27b63ac2c70d823704d5e73f27e07e6376
zhenzenghui <zhenzenghui@souche.com>
2019-02-02 23:21:55 +0800

    初始提交

---
+++ src/main/java/com/zzh/Test1.java
@@ -0,0 +1,13 @@
+package src.main.java.com.zzh;
+
+/**
+ * @author zzh
+ * @date 2019/2/2
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
