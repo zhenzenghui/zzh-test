94baf411ce3bb040e4669c260da7c677100452bf
zhenzenghui <zhenzenghui@souche.com>
2019-03-18 15:23:24 +0800

    test

---
+++ src/main/java/com/zzh/test/Test2.java
@@ -0,0 +1,11 @@
+package src.main.java.com.zzh.test;
+
+/**
+ * @author zzh
+ * @date 2019/3/18
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
